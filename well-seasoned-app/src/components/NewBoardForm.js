9961aba8911a25054716a7e887dc7541d5c2722f
Liliana <liliana.m.parra@gmail.com>
2022-07-01 19:20:17 -0400

    Added submit button to New Board Form

--- well-seasoned-app/src/components/NewBoardForm.js
+++ well-seasoned-app/src/components/NewBoardForm.js
@@ -17,23 +17,33 @@ const NewBoardForm = ({ onBoardDataReady }) => {
     const newFormData = { ...formData, [fieldName]: fieldValue };
     setFormData(newFormData);
   };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    onBoardDataReady(formData);
+    setFormData(kDefaultFormState);
+  };
+
   return (
     <div className="NewBoardForm">
-      <h3>Add a Board</h3>
-      <label>Board Title</label>
-      <input
-        type="text"
-        name="title"
-        value={formData.title}
-        onChange={handleChange}
-      ></input>
-      <label>Owner</label>
-      <input
-        type="text"
-        name="owner"
-        value={formData.owner}
-        onChange={handleChange}
-      ></input>
+      <form onSubmit={handleSubmit}>
+        <h3>Add a Board</h3>
+        <label>Board Title</label>
+        <input
+          type="text"
+          name="title"
+          value={formData.title}
+          onChange={handleChange}
+        ></input>
+        <label>Owner</label>
+        <input
+          type="text"
+          name="owner"
+          value={formData.owner}
+          onChange={handleChange}
+        ></input>
+        <input type="submit" value="Add Board"></input>
+      </form>
     </div>
   );
 };
