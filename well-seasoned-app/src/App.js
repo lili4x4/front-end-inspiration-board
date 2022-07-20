1380390d0e93ac71c9a7add933e29227b8726287
Joanna Dudley <jfdudleybmc@gmail.com>
2022-07-20 14:13:22 -0400

    Completed react router addition

--- well-seasoned-app/src/App.js
+++ well-seasoned-app/src/App.js
@@ -1,8 +1,6 @@
 import { useState, useEffect, useRef } from "react";
 import "./App.css";
 import BoardDropdown from "./components/BoardDropdown";
-import Board from "./components/Board";
-import NewCardForm from "./components/NewCardForm";
 import NewBoardForm from "./components/NewBoardForm";
 import axios from "axios";
 import React from "react";
@@ -90,39 +88,33 @@ function App() {
   const ref = useRef(null);
 
   const chooseBoard = (boardInfo) => {
-    console.log(boardInfo);
-    const filteredData = boardData.filter((data) => {
-      return data.board_id === boardInfo.board_id;
-    });
-    console.log(filteredData);
-    const board = filteredData[0];
-    setTheme(board);
-    setChosenBoard(board);
-    console.log(chosenBoard);
+    setChosenBoard(boardInfo);
   };
 
   const setTheme = (board) => {
-    const themeHeader = ref.current;
-    const themeBody = document.getElementsByTagName("body")[0];
-    console.log("ThemeBody is " + themeBody.current);
+    if (board.board_id !== 0) {
+      const themeHeader = ref.current;
+      const themeBody = document.getElementsByTagName("body")[0];
+      // console.log("ThemeBody is " + themeBody.current);
 
-    console.log("ThemeHeader is " + themeHeader.current);
-    console.log("chosenBoard.title=" + chosenBoard.title);
-    if (board.title === "Summer") {
-      themeHeader.setAttribute("id", "summer-header");
-      themeBody.setAttribute("id", "summer-body");
-    } else if (board.title === "Spring") {
-      themeHeader.setAttribute("id", "spring-header");
-      themeBody.setAttribute("id", "spring-body");
-    } else if (board.title === "Fall") {
-      themeHeader.setAttribute("id", "fall-header");
-      themeBody.setAttribute("id", "fall-body");
-    } else if (board.title === "Winter") {
-      themeHeader.setAttribute("id", "winter-header");
-      themeBody.setAttribute("id", "winter-body");
-    } else {
-      themeHeader.setAttribute("id", "default-header");
-      themeBody.setAttribute("id", "default-body");
+      // console.log("ThemeHeader is " + themeHeader.current);
+      // console.log("chosenBoard.title=" + chosenBoard.title);
+      if (board.title === "Summer") {
+        themeHeader.setAttribute("id", "summer-header");
+        themeBody.setAttribute("id", "summer-body");
+      } else if (board.title === "Spring") {
+        themeHeader.setAttribute("id", "spring-header");
+        themeBody.setAttribute("id", "spring-body");
+      } else if (board.title === "Fall") {
+        themeHeader.setAttribute("id", "fall-header");
+        themeBody.setAttribute("id", "fall-body");
+      } else if (board.title === "Winter") {
+        themeHeader.setAttribute("id", "winter-header");
+        themeBody.setAttribute("id", "winter-body");
+      } else {
+        themeHeader.setAttribute("id", "default-header");
+        themeBody.setAttribute("id", "default-body");
+      }
     }
   };
 
@@ -158,25 +150,26 @@ function App() {
     updateBoards();
 
     const themeHeader = "ref.current";
-    console.log("ThemeHeader is " + themeHeader.current);
+    // console.log("ThemeHeader is " + themeHeader.current);
 
     const themeBody = document.getElementsByTagName("body");
-    console.log("ThemeBody is " + themeBody.current);
+    // console.log("ThemeBody is " + themeBody.current);
   }, []);
 
   const createNewBoard = (newBoardData) => {
-    createNewBoardCallback(newBoardData)
+    return createNewBoardCallback(newBoardData)
       .then((newBoard) => {
-        setBoardData((oldData) => [...oldData, newBoard]);
+        const newData = [...boardData, newBoard];
+        setBoardData(newData);
         return newBoard;
       })
-      .then((newBoard) => {
-        chooseBoard(newBoard);
+      .then((returnBoard) => {
+        chooseBoard(returnBoard);
       });
   };
 
   const handleBoardDataReady = (formData) => {
-    createNewBoard(formData);
+    return createNewBoard(formData);
   };
 
   const createCard = (cardData, boardID) => {
@@ -199,14 +192,17 @@ function App() {
   };
 
   const handleCardDataReady = (cardData, boardID) => {
-    console.log("In handleCardDataReady");
     createCard(cardData, boardID);
   };
+
+  setTheme(chosenBoard);
 
   return (
     <BrowserRouter>
       <header id="default-header" ref={ref}>
-        <h1>Well-Seasoned</h1>
+        <Link to="/">
+          <h1>Well-Seasoned</h1>
+        </Link>
         <BoardDropdown boardData={boardData} chooseBoard={chooseBoard} />
         <Link to="/new-board">Add New Board</Link>
       </header>
--- well-seasoned-app/src/components/NewBoardForm.js
+++ well-seasoned-app/src/components/NewBoardForm.js
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import PropTypes from "prop-types";
 import "../App.css";
 import "./NewBoardForm.css";
-import { useNavigate } from "react-router-dom";
 
 const kDefaultFormState = {
   title: "",
